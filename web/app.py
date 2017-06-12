78b73d41e3f4419753ccbd77b0b8c176e7874022
Jatinder Singh <jatinder@Jatinders-MacBook-Pro.local>
2017-06-12 20:41:44 +0530

    Add the fb_token to main app

--- web/app.py
+++ web/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, request, session
 from personality_quotes.app import personality_quote
 from wwe_match_maker.app import match_maker
 
@@ -9,8 +9,29 @@ app.config.update(dict(SECRET_KEY='A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'))
 app.register_blueprint(personality_quote, url_prefix='/personality-quote')
 app.register_blueprint(match_maker, url_prefix='/match_maker')
 
+
+app_id = "1854877241500808"
+redirect_uri = 'http://localhost:5000/accept-fb-token'
+
+
 @app.route('/')
 def index():
-    home= {'personality': 'personality-quote/home', 'wwe': 'match_maker/home'}
+     login_url = 'https://www.facebook.com/v2.9/dialog/oauth?' \
+                + 'client_id=1854877241500808' \
+                + '&redirect_uri=' + redirect_uri \
+                + '&state=randomstring123' \
+                + '&response_type=token' \
+                + '&scope=public_profile,publish_actions,user_friends,email'
+     home= {'personality': 'personality-quote/confirm-quote', 'wwe': 'match_maker/confirm-pic'}
+     return render_template('index.html', home=home, login_url=login_url)
 
-    return render_template('index.html',home=home)
+
+@app.route('/accept-fb-token')
+def accept_fb_token():
+    if request.args.get('access_token'):
+        session['fb_token'] = request.args.get('access_token')
+
+        return redirect('/')
+
+    return render_template('fb-token.html')
+
